18fc631c128a85d50b83266d242ef1771983b990
vasukikumaresan <vasukikumaresan1211@gmail.com>
2024-12-06 14:21:46 +0530

    seleniumcode

---
+++ src/test/java/testcases/SeleniumJava1011TestCase.java
@@ -0,0 +1,29 @@
+package testcases;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import Utilities.TestBase;
+
+public class SeleniumJava1011TestCase extends TestBase{
+
+	@Test
+	public void SeleniumJava101_TC1() throws MalformedURLException, InterruptedException {
+		setUp("Chrome");
+		driver().get("https://www.lambdatest.com/selenium-playground/");
+
+		driver().findElement(By.xpath("//a[text()='Simple Form Demo']")).click();
+		Assert.assertTrue(driver().getCurrentUrl().contains("simple-form-demo"));
+		String text = "Welcome to LambdaTest";
+		driver().findElement(By.id("user-message")).sendKeys(text);
+		Thread.sleep(2000);
+		driver().findElement(By.id("showInput")).click();
+		String actualText = driver().findElement(By.cssSelector("[id='message']")).getText();
+		Assert.assertEquals(actualText, text);
+		driver().quit();
+		
+	}
+}
